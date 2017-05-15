b020525a766cf27aec7eb3053d36139d2ac3ff43
paul sparks <paulnderitu17@gmail.com>
2017-05-15 22:17:58 +0300

    removing chat

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-// $('.carousel').carousel();
+
+
+  var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+  (function() {
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5919e1ba4ac4446b24a6f329/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+  })();
